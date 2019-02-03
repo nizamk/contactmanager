7457a8fe0cbbd330e2530eed85b20a95532c2ff2
nizam.ariffin <nizam.ariffin@gmail.com>
2019-02-03 12:49:40 +0800

    Updates

---
+++ precache-manifest.d850e4ff688b3bd9ee9b0036a11ae4d2.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "72aa82900d090390d8dc",
+    "url": "/contactmanager/static/js/runtime~main.72aa8290.js"
+  },
+  {
+    "revision": "cf5d3f8aa074d8f54420",
+    "url": "/contactmanager/static/js/main.cf5d3f8a.chunk.js"
+  },
+  {
+    "revision": "7d13c42cb45dcc5e2bf4",
+    "url": "/contactmanager/static/js/1.7d13c42c.chunk.js"
+  },
+  {
+    "revision": "7d13c42cb45dcc5e2bf4",
+    "url": "/contactmanager/static/css/1.e52afd63.chunk.css"
+  },
+  {
+    "revision": "9876f73cf5d2cd51ca6b10cc107e526b",
+    "url": "/contactmanager/index.html"
+  }
+];
